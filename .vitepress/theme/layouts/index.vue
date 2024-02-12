7f8e4640468990d78fa01bd9c3eaf40145cada44
Lyric <5h3ll3x@gmail.com>
2024-02-11 16:44:28 -0800

    fix client only code

--- .vitepress/theme/layouts/index.vue
+++ .vitepress/theme/layouts/index.vue
@@ -1,102 +1,17 @@
 <script setup>
-import { onMounted, watch } from "vue";
-import { useRouter } from 'vitepress';
 import DefaultTheme from 'vitepress/theme'
+import SpeakWordInlineConverter from '../components/SpeakWordInlineConverter.vue'
 
 const { Layout } = DefaultTheme
 
-const router = useRouter();
-
-watch(() => router.route.data.relativePath, (newVal, oldVal) => {
-  if (newVal !== oldVal) {
-    setTimeout(() => {
-      buildSpeakWordInline();
-    }, 200);
-  }
-}, { immediate: true });
-
-function buildPlayButton(parent, accent, url) {
-  const labelEl = document.createElement('span');
-  labelEl.classList.add('accent-label');
-  labelEl.innerText = accent.toUpperCase();
-  const audioEl = document.createElement('audio');
-  audioEl.classList.add('audio')
-  audioEl.setAttribute('src', url)
-  audioEl.setAttribute('controls', 'false')
-  const iconEl = document.createElement('img');
-  iconEl.classList.add('icon');
-  let svg = '/images/speaker-blue.svg';
-  if (accent === 'uk') {
-    svg = '/images/speaker-brown.svg';
-  }
-  iconEl.setAttribute('src', svg)
-  iconEl.innerText = accent.toUpperCase();
-
-  const btnEl = document.createElement('button')
-  btnEl.classList.add('play-button')
-  btnEl.classList.add(accent);
-  btnEl.addEventListener('click', () => {
-    audioEl.play();
-  })
-  // btnEl.append(labelEl)
-  btnEl.append(iconEl)
-  btnEl.append(audioEl)
-  parent.append(btnEl)
-}
-
-function convertToInlineComponent(el) {
-  const word = el.innerText;
-  const dataPos = el.getAttribute('data-pos')
-  const dataAudioUs = el.getAttribute('data-audio-us')
-  const dataAudioUk = el.getAttribute('data-audio-uk')
-  console.log('inline component', dataAudioUs, dataAudioUk)
-
-  const wrapperEl = document.createElement('div')
-  wrapperEl.classList.add('speak-word-wrapper')
-  const canEl = document.createElement('div')
-  canEl.classList.add('speak-word')
-  canEl.classList.add('inline')
-
-  if (dataAudioUk || dataAudioUs) {
-    const ctrlEl = document.createElement('div')
-    ctrlEl.classList.add('ctrl')
-    const ctrlPartEl = document.createElement('div')
-    ctrlPartEl.classList.add('ctrl-part')
-    if (dataAudioUs) {
-      buildPlayButton(ctrlPartEl, 'us', dataAudioUs)
-    }
-    ctrlEl.append(ctrlPartEl);
-    const ctrlPartEl2 = document.createElement('div')
-    ctrlPartEl2.classList.add('ctrl-part')
-    if (dataAudioUk) {
-      buildPlayButton(ctrlPartEl2, 'uk', dataAudioUk)
-    }
-    ctrlEl.append(ctrlPartEl2);
-    canEl.append(ctrlEl)
-  }
-  wrapperEl.append(canEl)
-  // insert at el's side
-  el.insertAdjacentElement('afterend', wrapperEl)
-  el.style.display = 'none'
-}
-
-function buildSpeakWordInline() {
-  const inlinePlayerEls = document.querySelectorAll('.speak-word-inline')
-  for (let i = 0; i < inlinePlayerEls.length; i += 1) {
-    convertToInlineComponent(inlinePlayerEls[i]);
-  }
-}
-
-onMounted(() => {
-  setTimeout(() => {
-    buildSpeakWordInline();
-  }, 200)
-})
 </script>
 
 <template>
   <Layout>
     <template #doc-after>
+      <ClientOnly>
+        <SpeakWordInlineConverter />
+      </ClientOnly>
     </template>
   </Layout>
 </template>
